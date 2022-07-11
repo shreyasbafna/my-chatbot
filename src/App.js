30c4e8247e6a1eb035ebd27d941e61154b4426dc
shreyasbaf <shreyas7bafna@gmail.com>
2022-07-11 13:17:41 +0530

    added GTAGs body and head scripts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import TagManager from 'react-gtm-module'
 
 function App() {
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KJBRDM7'
+}
+
+TagManager.initialize(tagManagerArgs)
   return (
     <div className="App">
       <header className="App-header">
