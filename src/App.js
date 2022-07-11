231dadf67a8302a25c15bfe5a9b18618f01e6237
shreyasbaf <shreyas7bafna@gmail.com>
2022-07-11 13:36:21 +0530

    added button

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ function App() {
 const tagManagerArgs = {
     gtmId: 'GTM-KJBRDM7'
 }
-
 TagManager.initialize(tagManagerArgs)
+
+const purchaseNft = () => {
+  console.log('here')
+  const tagManagerArgs = {
+    gtmId: 'GTM-KJBRDM7',
+    dataLayer:{
+     event: 'buynft',
+    }
+}
+TagManager.dataLayer(tagManagerArgs)
+}
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Demo For Live Chat Bot / GTAG
         </p>
+        <button className='buyNFT' onClick={() => purchaseNft()}>
+          Buy NFT
+        </button>
       </header>
     </div>
   );
